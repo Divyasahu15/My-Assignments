a3fd5dc3dd0973bb6c123e1df622afbee5f9d2c8
Divya sahu <101404762+Divyasahu15@users.noreply.github.com>
2022-03-14 15:00:36 +0530

    Update A1.java

--- Corejava/Assignment3/Q1/A1.java
+++ Corejava/Assignment3/Q1/A1.java
@@ -1,48 +1,17 @@
 package a3;
-import java.util.*;
-
-
-	import java.io.BufferedInputStream;
-	import java.io.BufferedOutputStream;
-	import java.io.FileInputStream;
-	import java.io.FileNotFoundException;
-	import java.io.FileOutputStream;
-	import java.io.FileReader;
-	import java.io.FileWriter;
-	import java.io.IOException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 	public class A1 {
 
 		public static void main(String[] args) {
 
-			long start = System.currentTimeMillis();
+			
 			
 			try {
-				FileInputStream fr = new 
-					 FileInputStream("D:\\lectures\\Gate\\power system");
-			FileOutputStream fw = new FileOutputStream("D:\\lectures\\Gate\\power system");
-			
-				BufferedInputStream fr1 = new BufferedInputStream(new FileInputStream("D:\\lectures\\Gate\\power system"));
-				BufferedOutputStream fw1 = new BufferedOutputStream(new FileOutputStream("D:\\lectures\\Gate\\power system"));
-				int i = 0;
-				while ((i = fr1.read()) != -1) {
-					fw.write(i);
-			}
-			fw.close();
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-				e.printStackTrace();
-	}
-	
-         	long end = System.currentTimeMillis();
-	
-		System.out.println("time taken : " + (end - start) + " ms");
-			
-			try {
-				FileReader fr1=new FileReader("D:\\lectures\\Gate\\power system");
-				FileWriter fw1=new FileWriter("D:\\lectures\\Gate\\power system");
+				FileReader fr1=new FileReader("C:\Users\chira\Downloads\ronaldo - Google Search_files");
+				FileWriter fw1=new FileWriter("C:\Users\chira\Downloads\ronaldo - Google Search_files");
 				int i=0;
 				while((i=fr1.read())!=-1) {
 					fw1.write(i);
@@ -59,5 +28,3 @@ import java.util.*;
 			System.out.println("file is written");
 		}
 	}
-
-
